5158662317c0a5a15368c722448e289b3b3b7ded
Dead-TR <d.deadtr@gmail.com>
2024-02-15 20:51:07 +0200

    create game menu

--- src/components/Menu/pages/MainMenu/index.tsx
+++ src/components/Menu/pages/MainMenu/index.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 
 import { BG } from "components";
 import { usePath } from "providers";
@@ -7,15 +7,27 @@ import Play from "assets/play.svg";
 import Shop from "assets/shop.svg";
 
 import css from "./style.module.css";
+import { progressManager } from "managers";
 
 interface Props {}
 
 export const MainMenu: FC<Props> = ({}) => {
   const { page } = usePath();
+  const [coins, setCoins] = useState(() => progressManager.getters.coins());
+
+  useEffect(() => {
+    const rm = progressManager.listeners.coins((v) => setCoins(v));
+    return () => rm();
+  }, []);
+
   return (
     <BG>
       <div className={css.root}>
-        <div className={css.top}></div>
+        <div className={css.top}>
+          <span className={css.coins}>
+          Coins: {coins}
+          </span>
+        </div>
 
         <div className={css.center}>
           <div className={css.left}>
